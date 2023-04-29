c21ecc93f9b4bd881f4da96c180cd1a7171c6500
S1xZ <punya.gnwdn@gmail.com>
2023-04-29 17:43:08 +0700

    🔌Changed to only sends joined group message to joined client socket

--- server/server.py
+++ server/server.py
@@ -19,7 +19,8 @@ class ChatServer:
         while True:
             client_socket, client_address = self.socket.accept()
             client_thread = threading.Thread(
-                target=self.handle_client, args=(client_socket, ))
+                target=self.handle_client, args=(client_socket,)
+            )
             client_thread.start()
             print(f"Total Client: {threading.active_count() - 1}\n")
 
@@ -41,10 +42,14 @@ class ChatServer:
                 nickname = message["data"]
 
                 if nickname in self.clients:
-                    socket.send(json.dumps({
-                        "type": "username",
-                        "isExist": True,
-                    }).encode())
+                    socket.send(
+                        json.dumps(
+                            {
+                                "type": "username",
+                                "isExist": True,
+                            }
+                        ).encode()
+                    )
                     continue
 
                 self.clients[nickname] = socket
@@ -56,77 +61,98 @@ class ChatServer:
             elif message["type"] == "join":
                 group_name = message["data"]
 
-                if group_name not in self.chat_groups:
-                    self.update_groups(isExist=False, isJoin=True)
-                    continue
-
-                self.chat_groups[group_name].append(socket)
+                self.chat_groups[group_name].append(nickname)
                 print(f"Client {nickname} joined group {group_name}")
-                self.update_groups(isExist=True, isJoin=True)
+                socket.send(
+                    json.dumps(
+                        {
+                            "type": "groups",
+                            "isExist": True,
+                            "isJoin": True,
+                            "data": list(self.chat_groups.keys()),
+                        }
+                    ).encode()
+                )
 
             # Each client can create a chat group(s) and join the chat group(s).
             elif message["type"] == "create":
                 group_name = message["data"]
-                if group_name not in self.chat_groups:
-                    self.chat_groups[group_name] = [socket]
-                else:
-                    self.update_groups(isExist=False, isJoin=False)
+
+                if group_name in self.chat_groups:
+                    self.update_groups(isExist=True, isJoin=False)
                     continue
 
+                self.chat_groups[group_name] = [nickname]
                 print(f"Client {nickname} created group {group_name}")
                 self.update_groups(isExist=True, isJoin=False)
 
             # Each client can send a message to a chat group.
             elif message["type"] == "group_message":
                 group_name = message["group"]
+
                 message = message["data"]
                 print(
-                    f"Client {nickname} sent message to group {group_name}: {message}")
-                self.send_group_message(group_name, nickname, socket, message)
+                    f"Client {nickname} sent message to group {group_name}: {message}"
+                )
+                self.send_group_message(group_name, nickname, message)
 
             # Each client can send a direct message to other clients in the list.
             elif message["type"] == "direct_message":
                 recipient = message["recipient"]
                 message = message["data"]
-                print(
-                    f"Client {nickname} sent message to {recipient}: {message}")
+
+                print(f"Client {nickname} sent message to {recipient}: {message}")
                 self.send_direct_message(recipient, nickname, message)
 
-    def send_group_message(self, group_name, sender, sender_socket, message):
+    def send_group_message(self, group_name, sender, message):
         for nickname in self.clients:
-            if nickname in self.chat_groups[group_name] and self.clients[nickname] != sender_socket:
-                self.clients[nickname].send(json.dumps({
-                    "type": "group_message",
-                    "group": group_name,
-                    "sender": sender,
-                    "data": message
-                }).encode())
+            if nickname in self.chat_groups[group_name] and nickname != sender:
+                print(nickname)
+                socket = self.clients[nickname]
+                socket.send(
+                    json.dumps(
+                        {
+                            "type": "group_message",
+                            "group": group_name,
+                            "sender": sender,
+                            "data": message,
+                        }
+                    ).encode()
+                )
 
     def send_direct_message(self, recipient, sender, message):
         for nickname in self.clients:
             if nickname == recipient:
-                self.clients[nickname].send(json.dumps({
-                    "type": "direct_message",
-                    "sender": sender,
-                    "data": message
-                }).encode())
+                self.clients[nickname].send(
+                    json.dumps(
+                        {"type": "direct_message", "sender": sender, "data": message}
+                    ).encode()
+                )
 
     def update_users(self):
         for socket in self.clients.values():
-            socket.send(json.dumps({
-                "type": "users",
-                "data": list(self.clients.keys()),
-                "group_data": list(self.chat_groups.keys())
-            }).encode())
+            socket.send(
+                json.dumps(
+                    {
+                        "type": "users",
+                        "data": list(self.clients.keys()),
+                        "group_data": list(self.chat_groups.keys()),
+                    }
+                ).encode()
+            )
 
     def update_groups(self, isExist=False, isJoin=False):
         for socket in self.clients.values():
-            socket.send(json.dumps({
-                "type": "groups",
-                "isExist": isExist,
-                "isJoin": isJoin,
-                "data": list(self.chat_groups.keys())
-            }).encode())
+            socket.send(
+                json.dumps(
+                    {
+                        "type": "groups",
+                        "isExist": isExist,
+                        "isJoin": isJoin,
+                        "data": list(self.chat_groups.keys()),
+                    }
+                ).encode()
+            )
 
 
 if __name__ == "__main__":
